1ab0761dcc2fb5d8adb30268d90e218183d959c8
DOCENTE <docente@LC061500.local>
2022-05-31 20:08:36 -0400

    ingrese verificacion de edad

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,9 @@
 print("Hola mundo")
 nom = input("Ingrese su nombre: ")
 print("su nombre es: ",nom)
+edad = int(input("Ingrese su edad: "))
+if edad > 18:
+    print("Ud es mayor de edad")
+else:
+    print("Ud es un niño")
+    
