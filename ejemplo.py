e38b8047594a443a42a1266c5db5412427b44314
DOCENTE <docente@LC061500.local>
2022-06-01 21:37:34 -0400

    se agregaron funciones

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,4 @@
+'''
 print("Hola mundo")
 nom = input("Ingrese su nombre: ")
 print("su nombre es: ",nom)
@@ -6,4 +7,22 @@ if edad > 18:
     print("Ud es mayor de edad")
 else:
     print("Ud es un niño")
-    
+'''
+
+def sumar(a,b):
+    suma = a + b
+    print("la suma es: ", suma)
+# fin funcion
+
+def sumar2(a,b):
+    suma = a + b
+    return suma
+# fin funcion
+
+
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+sumar(num1,num2)
+suma2 = sumar2(num1,num2)
+print("la suma 2 es: ",suma2)
